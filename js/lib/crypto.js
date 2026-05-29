5f927fa5d1aacde2885f25ef6bbb37c72607cd7c
rdarshan927 <rdarshan927@gmail.com>
2026-05-30 00:54:31 +0530

    first commit v0.1.4

--- js/lib/crypto.js
+++ js/lib/crypto.js
@@ -1,4 +1,9 @@
-import * as ed from 'https://cdn.jsdelivr.net/npm/@noble/ed25519@2.2.3/+esm';
+import * as ed from 'https://esm.sh/@noble/ed25519@2.2.3';
+import { sha512 } from 'https://esm.sh/@noble/hashes@1.4.0/sha512';
+
+// Set fallbacks for environments without crypto.subtle or for internal sync requirements
+ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
+ed.etc.sha512Async = (...m) => Promise.resolve(ed.etc.sha512Sync(...m));
 
 // Utility to convert hex to bytes
 function hexToBytes(hex) {
@@ -16,9 +21,9 @@ function bytesToHex(bytes) {
 
 const CryptoUtils = {
     async generateKeypair() {
-        const privKey = ed.utils.randomPrivateKey();
+        const privKey = crypto.getRandomValues(new Uint8Array(32));
         const pubKey = await ed.getPublicKey(privKey);
-        const did = `did:key:z${bytesToHex(pubKey)}`; // simplified did:key format
+        const did = `did:key:${bytesToHex(pubKey)}`; // Keep consistent with backend stripping 'z'
         return {
             privateKey: bytesToHex(privKey),
             publicKey: bytesToHex(pubKey),
